dd2485fb4765cffc74b20f207b1f0d3879aa0d42
VARGHESE P ROY <125295191+Varghese254@users.noreply.github.com>
2024-11-06 11:54:33 +0530

    events second example

--- scripts.js
+++ scripts.js
@@ -55,18 +55,39 @@
 // emitter.emit("logout","user logged out");
 
 
-const EventEmmiter=require("events");
-const emitter=new EventEmmiter();
-const user="user1"
-emitter.on("message",(data)=>{
-    console.log(data);
-});
-   emitter.on("logout",(data)=>
-{
-    console.log(data);
-});
-emitter.emit("message",`Hello",${user}`);
-emitter.emit("logout","user logged out");
+// const EventEmmiter=require("events");
+// const emitter=new EventEmmiter();
+// const user="user1"
+// emitter.on("message",(data)=>{
+//     console.log(data);
+// });
+//    emitter.on("logout",(data)=>
+// {
+//     console.log(data);
+// });
+// emitter.emit("message",`Hello",${user}`);
+// emitter.emit("logout","user logged out");
 
 
+
+const EventEmitter = require("events");
+const emitter = new EventEmitter();
+
+emitter.on("request", (req) => {
+    console.log(`Received request for ${req.url}`);
+    if (req.url === "/error") {
+        emitter.emit("error", new Error("An error occurred"));
+    } else {
+        console.log(`Processing request for ${req.url}`);
+    }
+});
+
+emitter.on("error", (err) => {
+    console.log(`Error occurred: ${err.message}`);
+});
+
+// Emit the 'request' event instead of 'message'
+emitter.emit("request", { url: "/home" });
+emitter.emit("request", { url: "/about" });
+emitter.emit("request", { url: "/error" });
 
